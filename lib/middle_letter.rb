6199124f466ce04856435bd2b605e363a6fc2613
Patricia <prmonday@gmail.com>
2020-11-12 12:02:10 +0000

    4 test cases and implementation

--- lib/middle_letter.rb
+++ lib/middle_letter.rb
@@ -1,3 +1,7 @@
 def middle_letter(input_word)
-return 't'
+  if input_word.length.even?
+     return input_word[input_word.length/2-1] + input_word[input_word.length/2]
+  else
+     return input_word[input_word.length/2]
+  end
 end
--- spec/middleletter_spec.rb
+++ spec/middleletter_spec.rb
@@ -4,6 +4,20 @@ require 'middle_letter'
 describe ' middle_letter' do
   it 'takes a word and if the word length is odd it returns the middle letter'do
     word = middle_letter('testing')
-    expect(word).to eq( 't')
+    expect(word).to eq('t')
   end
-end 
+
+  it 'takes a word and if the word length is even it returns the middle 2 letters.'do
+    word = middle_letter('test')
+    expect(word).to eq('es')
+  end
+  it ' takes a character and return the same character'do
+   word = middle_letter('A')
+   expect(word).to eq('A')
+ end
+
+ it 'takes a word with 2 characters and return the same one.'do
+   word = middle_letter('of')
+   expect(word).to eq('of')
+ end
+end
